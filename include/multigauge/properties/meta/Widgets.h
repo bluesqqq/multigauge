eac9c4b3466bf7406ed11feefa4bab4ccdee0fd1
bluesqqq <bluesq.main@gmail.com>
2026-09-12 11:40:51 -0400

    feat: single source of truth for the widgets

---
+++ include/multigauge/properties/meta/Widgets.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace mg::widget {
+
+inline constexpr const char* text = "text";
+inline constexpr const char* number = "number";
+inline constexpr const char* boolean = "boolean";
+inline constexpr const char* select = "select";
+inline constexpr const char* json = "json";
+inline constexpr const char* color = "color";
+inline constexpr const char* gradient = "gradient";
+inline constexpr const char* valueRef = "value-ref";
+inline constexpr const char* layoutSize = "layout-size";
+inline constexpr const char* tickLevels = "tick-levels";
+inline constexpr const char* collection = "collection";
+inline constexpr const char* directionToggle = "direction-toggle";
+inline constexpr const char* alignmentGrid = "alignment-grid";
+inline constexpr const char* insets = "insets";
+inline constexpr const char* anchorPair = "anchor-pair";
+inline constexpr const char* axisPair = "axis-pair";
+
+} // namespace mg::widget
